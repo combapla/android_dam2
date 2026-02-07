133af5213291f644da912ed5aac673a40e12a6e5
Jaime <jahtabares2000@gmail.com>
2026-02-07 17:02:53 +0100

    cambios login

--- app/src/main/java/com/example/proyectoandroid/login/LoginViewModel.kt
+++ app/src/main/java/com/example/proyectoandroid/login/LoginViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.proyectoandroid.login
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.proyectoandroid.data.model.UsuarioEntity
+import com.example.proyectoandroid.data.repository.UsuarioRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+
+class LoginViewModel(
+    private val repository: UsuarioRepository
+) : ViewModel() {
+
+    private val _usuario = MutableStateFlow<UsuarioEntity?>(null)
+    val usuario: StateFlow<UsuarioEntity?> = _usuario
+
+    fun login(correo: String, password: String) {
+        viewModelScope.launch {
+            _usuario.value = repository.login(correo, password)
+        }
+    }
+}
+
+
