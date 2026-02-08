514493b0b694ef74c4d219a83626949d905e0fbd
pigeonh0lder <144426908+pigeonh0lder@users.noreply.github.com>
2026-02-08 14:37:07 +0100

    Nuevos cambios añadidos en el dao

--- app/src/main/java/com/example/proyectoandroid/data/dao/ProductoDao.kt
+++ app/src/main/java/com/example/proyectoandroid/data/dao/ProductoDao.kt
@@ -1,4 +1,25 @@
 package com.example.proyectoandroid.data.dao
 
-class ProductoDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.proyectoandroid.data.model.ProductoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ProductoDao {
+    @Insert(onConflict = androidx.room.OnConflictStrategy.REPLACE)
+    suspend fun insertarProducto(producto: ProductoEntity)
+
+    @androidx.room.Update
+    suspend fun actualizarProducto(producto: ProductoEntity)
+
+    @androidx.room.Delete
+    suspend fun eliminarProducto(producto: ProductoEntity)
+
+    @Query("SELECT * FROM productos")
+    fun getProductos(): Flow<List<ProductoEntity>>
+
+    @Query("SELECT * FROM productos WHERE id = :id")
+    suspend fun getProductoPorId(id: Int): ProductoEntity?
 }
